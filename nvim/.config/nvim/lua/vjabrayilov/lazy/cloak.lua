3d325d4523be705a9153ed17478c597069b0bc6d
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-02-04 18:42:46 -0500

    nit on formatting Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- nvim/.config/nvim/lua/vjabrayilov/lazy/cloak.lua
+++ nvim/.config/nvim/lua/vjabrayilov/lazy/cloak.lua
@@ -1,6 +1,6 @@
 return {
     "laytan/cloak.nvim",
-    config = function() 
+    config = function()
         require("cloak").setup({
             enabled = true,
             cloak_character = "*",
