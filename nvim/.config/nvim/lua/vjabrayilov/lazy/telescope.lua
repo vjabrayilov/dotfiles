0a5fed587af70ff36d3d6c49d0f3b4d76cd50973
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-02-05 14:19:14 -0500

    bump the version of telescope
    Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- nvim/.config/nvim/lua/vjabrayilov/lazy/telescope.lua
+++ nvim/.config/nvim/lua/vjabrayilov/lazy/telescope.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+    tag = "0.1.8",
 
     dependencies = {
         "nvim-lua/plenary.nvim"
@@ -31,7 +31,6 @@ return {
         end)
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         -- <C-p> git files
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     end
 }
