551266f619bc89a3e0c8f3c27d341241f64e40aa
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-02-05 16:35:28 -0500

    better general settings for now
    Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- nvim/.config/nvim/lua/vjabrayilov/set.lua
+++ nvim/.config/nvim/lua/vjabrayilov/set.lua
@@ -1,32 +1,47 @@
+-- General Settings
 vim.opt.guicursor = ""
-
 vim.opt.nu = true
+
 vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+--" Decent wildmenu
+-- in completion, when there is more than one match,
+-- list all matches, and only complete to longest common match
+vim.opt.wildmode = 'list:longest'
+
+-- case-insensitive search/replace
+vim.opt.ignorecase = true
+-- unless uppercase in search term
+vim.opt.smartcase = true
+-- more better diffs ignoring whitespace
+vim.opt.diffopt:append('iwhite')
+vim.opt.diffopt:append('algorithm:histogram')
+vim.opt.diffopt:append('indent-heuristic')
+
+vim.opt.colorcolumn = "80"
+vim.api.nvim_create_autocmd('Filetype', { pattern = 'rust', command = 'set colorcolumn=100' })
+
+vim.opt.listchars = 'tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•'
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
 vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 2
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
