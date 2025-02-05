c98339fc45d82cdb46eb5d43948be3c4138a1507
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-02-04 21:04:20 -0500

    improve nvim status bar
    Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- nvim/.config/nvim/lua/vjabrayilov/lazy/lualine.lua
+++ nvim/.config/nvim/lua/vjabrayilov/lazy/lualine.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('lualine').setup({
-        options = {theme='material'},
+        options = {theme='powerline', section_separators = '', component_separators = ''},
         })
 
     end
