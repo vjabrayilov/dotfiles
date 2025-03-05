b1892d1f18fae13499aca664c7275a3a3b39e3b7
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-03-04 19:05:46 -0500

    remap binding for nvim Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- nvim/.config/nvim/lua/vjabrayilov/remap.lua
+++ nvim/.config/nvim/lua/vjabrayilov/remap.lua
@@ -1,61 +1,54 @@
-
+-- vim.keymap.set("n", "<Space>", "<Nop>", {silent = true})
 vim.g.mapleader = " "
+-- Open file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ`z")
+-- Up/Down by centering the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Search and center
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
--- greatest remap ever
+-- On Visual Mode paste without losing the clipboard
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- Move selection up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Join lines without losing the cursor position
+vim.keymap.set("n", "J", "mzJ`z")
+-- Copy to the system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- Copy to the system clipboard but full line
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
+-- Delete w/o yanking
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+-- C-c = Esc
 vim.keymap.set("i", "<C-c>", "<Esc>")
-
+-- Q = nop
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- Format the buffer
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
+-- Prev/Next on quickfix/location lists
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- Replace word under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/vjabrayilov/packer.lua<CR>");
+-- Make it rain (garbage)
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
+-- Source the current file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
-vim.keymap.set('n', '<C-x>', ':split<CR>') -- ctrl + x to split horizontally
-vim.keymap.set('n', '<C-v>', ':vsplit<CR>') -- ctrl + v to split vertically
-
-vim.keymap.set('n', '<leader>g', ':Neogit<CR>') -- SPACE + g to open neogit
+-- Split horizontally/vertically
+vim.keymap.set('n', '<C-x>', ':split<CR>')
+vim.keymap.set('n', '<C-v>', ':vsplit<CR>')
+-- Neogit
+vim.keymap.set('n', '<leader>g', ':Neogit<CR>')
+-- Save quickly
+vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
+-- Jump to start and end of line using the home row keys
+vim.keymap.set('', 'H', '^')
+vim.keymap.set('', 'L', '$')
+-- Open new file adjacent to current file
+vim.keymap.set('n', '<leader>o', ':e <C-R>=expand("%:p:h") . "/" <cr>')
