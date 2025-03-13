4f868ed1e3e67c1bc5deb8f0e7d57744e83dfece
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-03-13 15:58:04 -0400

    fix typo Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -23,7 +23,7 @@ function check_dependencies {
     sudo apt-get update > /dev/null 2>&1 || echo "   ❌ Error: Failed to update package list"
     sudo apt-get upgrade -y  > /dev/null 2>&1 || echo "   ❌ Error: Failed to upgrade package list"
     for cmd in "${required_cmds[@]}"; do
-        if ! command -v "$cmd" &> /dev/null || then
+        if ! command -v "$cmd" &> /dev/null; then
             if  ![[ "$cmd" == "python3.10-venv" ]]; then
                 echo "   🔄 Installing missing dependency: $cmd"
             fi
