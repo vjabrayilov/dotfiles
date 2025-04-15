abf18f79d3799e886c7078e90eb29aa1c0201be3
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-04-15 18:11:30 +0000

    nit

--- setup2.sh
+++ setup2.sh
@@ -7,12 +7,16 @@ function usage {
 	echo "  dev               Setup a development environment."
 	echo "  virt              Setup a virtualization environment."
 	echo "  llvm [VERSION]    Setup LLVM toolchain (defaults to version 20 if not specified)."
+    echo "  kernel            Setup a kernel development environment."
+    echo "  rust              Setup a Rust development environment."
 	echo "  help              Show this message."
 }
 
 source scripts/dev.sh
 source scripts/virtualization.sh
 source scripts/llvm.sh
+source scripts/kernel.sh
+source scripts/rust.sh
 
 if [ $# -lt 1 ]; then
 	usage
@@ -39,6 +43,12 @@ case "$MODE" in
 	fi
 	setup_llvm "$VERSION"
 	;;
+"kernel")
+    setup_kernel_env
+    ;;
+"rust")
+    setup_rust
+    ;;
 "help")
 	usage
 	;;
