6dc0aed5dfff4d89f52f6a58debdbe5cc7dc7f99
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-04-15 15:59:43 +0000

    virt: prettify and lint

--- scripts/virtualization.sh
+++ scripts/virtualization.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    echo "This script should be sourced, not executed directly."
-    exit 1
+	echo "This script should be sourced, not executed directly."
+	exit 1
 fi
 
 function install_dependencies {
-    echo "1. Installing required dependencies..."
-    deps=("virtme-ng" "socat" "virtiofsd"  "qemu-system-x86" "qemu-kvm" "libvirt-daemon-system" "libguestfs-tools" "virt-manager")
-    sudo apt-get update > /dev/null 2>&1 || echo "   ❌ Error: Failed to update package list"
-    sudo apt-get upgrade -y > /dev/null 2>&1 || echo "   ❌ Error: Failed to upgrade package list"
-    for dep in "${deps[@]}"; do
-        if ! dpkg -l | grep -q "$dep"; then
-            echo "   🔄 Installing missing dependency: $dep"
-            sudo apt-get install -y "$dep" > /dev/null 2>&1 || echo "   ❌ Error: Failed to install $dep"
-        else
-            echo "   ✅ $dep is already installed."
-        fi
-    done
+	echo "1. Installing required dependencies..."
+	deps=("virtme-ng" "socat" "virtiofsd" "qemu-system-x86" "qemu-kvm" "libvirt-daemon-system" "libguestfs-tools" "virt-manager")
+	sudo apt-get update >/dev/null 2>&1 || echo "   ❌ Error: Failed to update package list"
+	sudo apt-get upgrade -y >/dev/null 2>&1 || echo "   ❌ Error: Failed to upgrade package list"
+	for dep in "${deps[@]}"; do
+		if ! dpkg -l | grep -q "$dep"; then
+			echo "   🔄 Installing missing dependency: $dep"
+			sudo apt-get install -y "$dep" >/dev/null 2>&1 || echo "   ❌ Error: Failed to install $dep"
+		else
+			echo "   ✅ $dep is already installed."
+		fi
+	done
 }
 
 function setup_virtualization {
-    echo "Setting up a virtualization environment..."
-    install_dependencies
-    sudo usermod -aG libvirt,kvm $USER
-    echo "After reboot, check kvm-ok and virt-host-validate"
+	echo "Setting up a virtualization environment..."
+	install_dependencies
+	sudo usermod -aG libvirt,kvm "$USER"
+	echo "After reboot, check kvm-ok and virt-host-validate"
 }
