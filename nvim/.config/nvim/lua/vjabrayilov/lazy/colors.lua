8e7d1c895f742efeb9869889cb339a9f24062c95
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-07-03 15:03:06 +0000

    nvim: add gruvbox Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- nvim/.config/nvim/lua/vjabrayilov/lazy/colors.lua
+++ nvim/.config/nvim/lua/vjabrayilov/lazy/colors.lua
@@ -1,24 +1,53 @@
 return {
     {
-        "folke/tokyonight.nvim",
+        -- "folke/tokyonight.nvim",
+        -- config = function()
+        --     require("tokyonight").setup({
+        --         -- your configuration comes here
+        --         -- or leave it empty to use the default settings
+        --         style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        --         transparent = true,     -- Enable this to disable setting the background color
+        --         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        --         styles = {
+        --             -- Style to be applied to different syntax groups
+        --             -- Value is any valid attr-list value for `:help nvim_set_hl`
+        --             comments = { italic = false },
+        --             keywords = { italic = false },
+        --             -- Background styles. Can be "dark", "transparent" or "normal"
+        --             sidebars = "dark", -- style for sidebars, see below
+        --             floats = "dark",   -- style for floating windows
+        --         },
+        --     })
+        --     vim.cmd("colorscheme delek")
+        -- end
+        -- Default options:
+        "ellisonleao/gruvbox.nvim",
         config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true,     -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark",   -- style for floating windows
-                },
+            require("gruvbox").setup({
+                    terminal_colors = true, -- add neovim terminal colors
+                --     undercurl = true,
+                --     underline = true,
+                --     bold = true,
+                    italic = {
+                        strings = true,
+                        emphasis = true,
+                        comments = true,
+                        operators = false,
+                        folds = true,
+                    },
+                --     strikethrough = true,
+                --     invert_selection = false,
+                --     invert_signs = false,
+                invert_tabline = true,
+                --     inverse = false, -- invert background for search, diffs, statuslines and errors
+                --     contrast = "",   -- can be "hard", "soft" or empty string
+                --     palette_overrides = {},
+                --     overrides = {},
+                --     dim_inactive = false,
+                    transparent_mode = true,
             })
-            vim.cmd("colorscheme delek")
+            -- vim.o.background = "light"
+            vim.cmd("colorscheme gruvbox")
         end
     },
 }
