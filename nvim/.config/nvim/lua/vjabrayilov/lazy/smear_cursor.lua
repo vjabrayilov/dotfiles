152806da8ae356403d4d259e21dc5c8b05f3d86d
s4nsec <s4nsec@gmail.com>
2025-08-03 16:05:19 -0400

    Add smear plugin for fancy custom animations

---
+++ nvim/.config/nvim/lua/vjabrayilov/lazy/smear_cursor.lua
@@ -0,0 +1,10 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        enabled = true,
+        smear_between_buffers = false,
+        stiffness = 0.9,
+        trailing_stiffness = 0.8,
+        distance_stop_animating = 0.3,
+    },
+}
